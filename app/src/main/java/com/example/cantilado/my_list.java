47e0f5381c785b2f54a25c5517d5fca092f10f86
mariloucantilado <nellahtwins2@gmail.com>
2023-03-04 21:14:45 +0800

    2-4-2023

--- app/src/main/java/com/example/cantilado/my_list.java
+++ app/src/main/java/com/example/cantilado/my_list.java
@@ -3,10 +3,12 @@ package com.example.cantilado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class my_list extends AppCompatActivity {
 
@@ -22,8 +24,17 @@ public class my_list extends AppCompatActivity {
         listView=(ListView) findViewById(R.id.listView);
         listItem = getResources().getStringArray(R.array.array_technology);
 
-        final ArrayAdapter<String> adapter = new ArrayAdapter(this,R.layout.activity_my_list, R.id.textView, listItem);
+        final ArrayAdapter<String> adapter = new ArrayAdapter(this,R.layout.mylist, R.id.textView, listItem);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                String value = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), value,Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
 
 
