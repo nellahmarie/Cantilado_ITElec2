0a260ba39424f1268666138a6c91003420f0aa72
Nellah <nellahtwins2@gmail.com>
2023-04-15 18:26:47 +0800

    April 15, 2023

--- app/myapplication/src/main/java/com/example/myapplication/SecondActivity.java
+++ app/myapplication/src/main/java/com/example/myapplication/SecondActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    private SecondActivity binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
